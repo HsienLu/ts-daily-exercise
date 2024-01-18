7abcf201fce679639b7d7466806def1478b484ce
HsienLu <hsienlu@mail.ntust.edu.tw>
2024-01-18 09:47:25 +0800

    day9 done

---
+++ Day 9 - interfaces 延伸功能介紹.ts
@@ -0,0 +1,16 @@
+/*
+哪一個關鍵字用於 TypeScript 中的介面擴展？
+B. extend
+
+當兩個擁有相同名稱的介面出現在同一作用域中時，TypeScript 會怎麼做？
+C. 合併它們
+
+
+在 TypeScript 中，介面合併（Interface Merging）通常用於什麼情況？
+B. 當需要向現有介面添加更多屬性
+
+
+下面哪種說法正確描述了 TypeScript 中的介面擴展（Interface Extending）？
+A. 它允許一個介面從另一個介面繼承屬性和方法。
+
+*/
